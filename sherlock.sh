2f0bbdcce634b5f47761f023e901ee721285339f
sidejackthenativity <70146213+sidejackthenativity@users.noreply.github.com>
2021-07-28 09:53:16 -0400

    Update sherlock.sh
    removed bracket at end

--- sherlock.sh
+++ sherlock.sh
@@ -2,5 +2,6 @@
 input="list.txt"
 while IFS= read -r line
 do
-  grep -R -oE ".{0,5}"$line"\(.{0,40}" --color=always
+  #grep -R -oE ".{0,5}"$line"\(.{0,40}" --color=always
+  grep -R -oE ".{0,5}"$line".{0,40}" --color=always
 done < "$input"
